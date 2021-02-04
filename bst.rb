265cc9aa9f889c8a474a08bdcc4457ddbfec000d
Nicholas <nicholasbarizo@gmail.com>
2021-02-04 02:46:12 -0800

    added pretty printer provided by TOP modified code so it works

--- bst.rb
+++ bst.rb
@@ -15,23 +15,48 @@ class Tree
     @root = build_tree(input)
   end
 
-  def build_tree(input)
-    puts "input: #{input}"
-    return nil if input.empty?
-    return Node.new(input, nil, nil) if input.length == 1
+  def sort(input)
+    sorted = true
+    input.each_with_index do |num, ind|
+      next if ind.zero?
 
-    if input.length == 2
-      p "Input: #{input}"
-      return Node.new(input[0], input[1], nil)
+      first = input[ind - 1]
+      second = num
+      next if first < second
+
+      sorted = false
+      input[ind] = first
+      input[ind - 1] = second
     end
+    input = sort(input) if sorted == false
+    input
+  end
 
-    data = input[input.length / 2]
-    left_node = build_tree(input.slice(0, input.length / 2))
-    right_node = build_tree(input.slice(input.length / 2 + 1, input.length))
-    Node.new(data, left_node, right_node)
+  def build_tree(array, i=0)
+    array = sort(array.uniq) if i.zero?
+    return Node.new(array[0], nil, nil) if array.length == 1
+    return Node.new(array[0], Node.new(array[1], nil, nil), nil) if array.length == 2 && array[1] < array[0]
+    return Node.new(array[0], nil, Node.new(array[1], nil, nil)) if array.length == 2
+
+    data = array[array.length / 2]
+    left_node = build_tree(array.slice(0, array.length / 2), i + 1)
+    right_node = build_tree(array.slice(array.length / 2 + 1, array.length), i + 1)
+    @root = Node.new(data, left_node, right_node)
+  end
+
+  def insert
+    @root
+  end
+
+  # Pretty print provided by The Odin Project
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
   end
 end
 
-test_tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
+test_tree = Tree.new([1, 9, 6, 8, 3, 4, 2, 1, 11])
 puts 'root:'
 p test_tree.root
+p test_tree.pretty_print
